d27b6e0b846ba8e1bcdb80e0ef5a17de2bcee5bb
emilalg <emil.algars@metropolia.fi>
2023-08-31 15:39:56 +0300

    eka :)

---
+++ src/interfaces/Cat.ts
@@ -0,0 +1,24 @@
+import {RowDataPacket} from 'mysql2';
+import {User} from './User';
+interface Cat {
+  // TODO: create a cat interface
+  // owner should be a User or a number
+  owner: User | number;
+  cat_id: number;
+  cat_name: string;
+  weight: number;
+  filename: string;
+  birthdate: Date;
+  lat: number;
+  lng: number;
+}
+
+interface GetCat extends RowDataPacket, Cat {}
+
+// TODO: create PostCat interface or type. Same as cat but without id
+type PostCat = Omit<Cat, 'cat_id'>;
+
+// TODO: create PutCat interface or type. Sameas PostCat but properties are optional
+type PutCat = Partial<PostCat>;
+
+export {Cat, GetCat, PostCat, PutCat};
